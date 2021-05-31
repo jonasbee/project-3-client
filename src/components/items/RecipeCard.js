b2c10488205349865e6171e12134e8803dd66e12
Abubakar Siddique <abz.siddique@gmail.com>
2021-05-31 13:03:11 +0100

    style: Add basic typography to recipe index

--- src/components/items/RecipeCard.js
+++ src/components/items/RecipeCard.js
@@ -2,29 +2,29 @@ function RecipeCard({ name, timeRequired, serves, instructionSteps, difficulty, 
   return (
     <div className="card">
       <div className="card-image">
-        <figure className="image is-4by3">
-          {/* // ! Replace image */}
+        <figure className="image is-3by2">
           <img src={image} alt={name} />
         </figure>
       </div>
       <div className="card-content">
-        <p>Name: {name}</p>
-        <p>Time Required: {timeRequired}</p>
-        <p>Serves: {serves}</p>
-        <br />
-        <p>Instructions: 
+        <h2 className="subtitle is-2 mb-4 is-capitalized has-text-weight-bold">{name}</h2>
+        <h5 className="subtitle is-5 mb-2 is-capitalized has-text-weight-bold">Time Required: <span className="has-text-weight-normal">{timeRequired}</span></h5>
+        <h5 className="subtitle is-5 mb-2 is-capitalized has-text-weight-bold">Serves: <span className="has-text-weight-normal">{serves}</span></h5>
+        <h5 className="subtitle is-5 mb-5 is-capitalized has-text-weight-bold">Difficulty: <span className="has-text-weight-normal">{difficulty}</span></h5>
+        <h5 className="subtitle is-5 has-text-weight-bold">Instructions: </h5>
+        <div>  
           {instructionSteps.map((step, i) => (
             <ul key="step">
-              <li>{i + 1}. {step}</li>
+              <li className="mb-2">{i + 1}. {step}</li>
             </ul>
           ))}
-        </p>
-        <p>Difficulty: {difficulty}</p>
+        </div>
         <br />
-        <div>Ingredients: 
+        <h5 className="subtitle is-5 has-text-weight-bold">Ingredients: </h5>
+        <div>
           {ingredients.map(ingredient =>
-            <ul key='name'>
-              <li>{ingredient.name} - {ingredient.number}{ingredient.metric}</li>
+            <ul key="name" className="is-capitalized">
+              <li>{ingredient.name} {ingredient.number}{ingredient.metric}</li>
             </ul>
           )}
         </div>      
