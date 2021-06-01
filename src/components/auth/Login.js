6115ddc934c7f4aedeb953c868e37b329fa1c2d4
Dimitar <dconev@gmail.com>
2021-06-01 12:41:25 +0100

    logo final version

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -40,7 +40,7 @@ function Login () {
               onSubmit={handleSubmit}
             >
               <div className="field">
-                <label className="label">Email</label>
+                <label className="label has-text-white">Email</label>
                 <div className="control">
                   <input
                     className="input"
@@ -52,7 +52,7 @@ function Login () {
                 </div>
               </div>
               <div className="field">
-                <label className="label">Password</label>
+                <label className="label has-text-white">Password</label>
                 <div className="control">
                   <input
                     type="password"
